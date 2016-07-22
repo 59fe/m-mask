c30bba7dc47fb89dddf4e611b22400c614106b0c
半边 <itaofe@gmail.com>
2016-07-22 16:26:07 +0800

    add mask comp

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _classnames = require('classnames');
+
+var _classnames2 = _interopRequireDefault(_classnames);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+
+var mask_prefix = 'mask';
+
+exports.default = function (props) {
+    var className = props.className;
+    var _props$style = props.style;
+    var style = _props$style === undefined ? {} : _props$style;
+
+    var others = _objectWithoutProperties(props, ['className', 'style']);
+
+    var maskClass = (0, _classnames2.default)(mask_prefix, className);
+
+    return _react2.default.createElement('div', _extends({}, others, { className: maskClass, style: style }));
+};
+
+module.exports = exports['default'];
