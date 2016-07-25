9724d046cbc886ea8a0558ba162c43457619bacc
半边 <itaofe@gmail.com>
2016-07-25 16:21:23 +0800

    import style in comp js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", {
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
+require("./index.less");
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
